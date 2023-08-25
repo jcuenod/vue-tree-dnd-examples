e6da3ae89ff78a1b295a78efdd933622521c4a48
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-08-25 18:19:58 -0500

    Add lock button and bump lib dep

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,81 @@
 <template>
-  <h1>Vue-Tree-Dnd Demo</h1>
-  <h2>Open source draggable tree component for Vue 3</h2>
-  <div style="text-align: center">
-    <!-- npm badge -->
-    <a href="https://www.npmjs.com/package/vue-tree-dnd"
-      ><img
-        src="https://img.shields.io/npm/v/vue-tree-dnd.svg?style=for-the-badge"
-        alt="npm version"
-        height="18"
-    /></a>
-    {{ " " }}
-    <!-- github (license) badge -->
-    <a href="https://github.com/jcuenod/vue-tree-dnd"
-      ><img
-        src="https://img.shields.io/github/license/jcuenod/vue-tree-dnd.svg?style=for-the-badge"
-        alt="npm version"
-        height="18"
-    /></a>
+  <div class="header">
+    <h1>Vue-Tree-Dnd Demo</h1>
+    <h2>Open source draggable tree component for Vue 3</h2>
+    <div style="text-align: center">
+      <!-- npm badge -->
+      <a href="https://www.npmjs.com/package/vue-tree-dnd"
+        ><img
+          src="https://img.shields.io/npm/v/vue-tree-dnd.svg?style=for-the-badge"
+          alt="npm version"
+          height="18"
+      /></a>
+      {{ " " }}
+      <!-- github (license) badge -->
+      <a href="https://github.com/jcuenod/vue-tree-dnd"
+        ><img
+          src="https://img.shields.io/github/license/jcuenod/vue-tree-dnd.svg?style=for-the-badge"
+          alt="npm version"
+          height="18"
+      /></a>
+    </div>
   </div>
   <div class="container">
-    <button @click="newFolder" class="new-button">
-      <span style="width: 1rem; height: 1rem"
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-          />
-        </svg>
-      </span>
-      <span> New Folder </span>
-    </button>
+    <div style="display: flex; flex-direction: row; justify-content: center">
+      <div style="flex: 1"></div>
+      <button @click="newFolder" class="new-button" style="justify-self: end">
+        <span style="width: 1rem; height: 1rem">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3.75 3A1.75 1.75 0 002 4.75v10.5c0 .966.784 1.75 1.75 1.75h12.5A1.75 1.75 0 0018 15.25v-8.5A1.75 1.75 0 0016.25 5h-4.836a.25.25 0 01-.177-.073L9.823 3.513A1.75 1.75 0 008.586 3H3.75zM10 8a.75.75 0 01.75.75v1.5h1.5a.75.75 0 010 1.5h-1.5v1.5a.75.75 0 01-1.5 0v-1.5h-1.5a.75.75 0 010-1.5h1.5v-1.5A.75.75 0 0110 8z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+        <span> New Folder </span>
+      </button>
+      <button @click="locked = !locked" class="lock-button">
+        <span style="width: 1rem; height: 1rem">
+          <svg
+            v-if="locked"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M14.5 1A4.5 4.5 0 0010 5.5V9H3a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-1.5V5.5a3 3 0 116 0v2.75a.75.75 0 001.5 0V5.5A4.5 4.5 0 0014.5 1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+      </button>
+      <div style="flex: 1"></div>
+    </div>
     <div style="padding-top: 1rem; padding-bottom: 1rem">
-      <VueTreeDnd :component="TreeItemRenderer" :tree="tree" @move="onMove" />
+      <VueTreeDnd
+        :component="TreeItemRenderer"
+        :tree="tree"
+        :locked="locked"
+        @move="onMove"
+      />
     </div>
   </div>
 
@@ -58,6 +95,7 @@ import { deleteNodeFromTree, moveMutation } from "./tree-helper";
 import { getRandomName } from "./naming-helper";
 
 const tree = ref<TreeItem[]>([]);
+const locked = ref<boolean>(false);
 
 const onMove = (event: MoveMutation) => {
   tree.value = moveMutation(tree.value, event);
@@ -105,9 +143,10 @@ h2 {
   border-radius: 0.5rem;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
-.new-button {
+
+.new-button,
+.lock-button {
   margin: 0 auto;
-  background-color: #60a5fa;
   border: none;
   border-radius: 0.3rem;
   padding: 0.25rem 1rem;
@@ -118,6 +157,11 @@ h2 {
   font-size: 1rem;
   font-weight: bold;
   color: #fff;
+  background-color: #60a5fa;
+}
+.new-button {
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
 }
 .new-button:hover {
   background-color: #3b82f6;
@@ -129,6 +173,21 @@ h2 {
 }
 .new-button span {
   margin: 0.2rem;
+}
+
+.lock-button {
+  background-color: #2563eb;
+  color: #dbeafe;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.lock-button:hover {
+  background-color: #1e40af;
+  color: #eff6ff;
+}
+.lock-button:active {
+  background-color: #172554;
+  color: #fff;
 }
 
 .footer {
